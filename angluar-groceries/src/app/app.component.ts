e7bddec7debe0bbfcdc695985dbee2a92f4ae4ad
David Stanich <dstanich@gmail.com>
2019-02-03 22:14:22 -0600

    feat(angular-groceries): add grocery component to app

--- angluar-groceries/src/app/app.component.ts
+++ angluar-groceries/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private dataService: DataService) {}
+
+  groceryAdded(name: string) {
+    this.dataService.addItem(name);
+  }
+}
