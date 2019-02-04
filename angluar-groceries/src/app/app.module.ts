6c35caf8765a4acf8fcd40ebe417720c8ef099ff
David Stanich <dstanich@gmail.com>
2019-02-03 22:05:33 -0600

    feat(angular-groceries): add add-grocery component

--- angluar-groceries/src/app/app.module.ts
+++ angluar-groceries/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { AddGroceryComponent } from './add-grocery/add-grocery.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TitleComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TitleComponent, AddGroceryComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
